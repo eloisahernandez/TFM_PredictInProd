fb95f77c5b97d2c565f5114ce5a7b832b62bee47
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-11-18 13:44:46 +0100

    Completed first challenge

---
+++ api/fast.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from datetime import datetime
+import pytz
+import joblib
+
+PATH_TO_LOCAL_MODEL = 'model.joblib'
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index():
+    return {"greeting": "Hello world"}
+
+@app.get("/predict")
+def predict(pickup_datetime, pickup_longitude, pickup_latitude,
+          dropoff_longitude, dropoff_latitude, passenger_count):
+
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+    # localize the user datetime with NYC timezone
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+
+    formatted_pickup_datetime = utc_pickup_datetime.strftime(
+        "%Y-%m-%d %H:%M:%S UTC")
+
+    X_pred = pd.DataFrame.from_dict({
+        "key": ["2013-07-06 17:18:00.000000119"],
+        "pickup_datetime": [formatted_pickup_datetime],
+        "pickup_longitude": [float(pickup_longitude)],
+        "pickup_latitude": [float(pickup_latitude)],
+        "dropoff_longitude": [float(dropoff_longitude)],
+        "dropoff_latitude": [float(dropoff_latitude)],
+        "passenger_count": [float(passenger_count)]
+    })
+    #print(X_pred)
+    pipeline = joblib.load(PATH_TO_LOCAL_MODEL)
+    y_pred = pipeline.predict(X_pred)
+
+    return {'prediction' : float(y_pred)}
